1cedfe1e3a2a2f55600eb8da231b084312842930
ParzivalOne <mateusdelg@gmail.com>
2020-03-01 19:55:28 -0300

    firebase setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 void main (){
-  runApp(MaterialApp(
 
-  ));
+  Firestore.instance.collection("col").document("doc").setData({"texto" : "parzival"});
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter demo",
+      theme: ThemeData(
+        primarySwatch: Colors.black,
+      ),
+      home: Container()
+    );
+  }
 }
