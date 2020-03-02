4f027943c82f32eb15e5a370ca6247dca2d3b8b4
ParzivalOne <mateusdelg@gmail.com>
2020-03-01 21:34:39 -0300

    test

---
+++ README.md
@@ -0,0 +1,7 @@
+# zipzop
+
+Online chat app made with Flutter using Firebase
+
+Made by Mateus Delgado
+
+# add your google-services.json file on android/app/ for the app to work
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:zipzop/chat_screen.dart';
+
+void main (){
+    runApp(MaterialApp(
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    ));
+
+/*    Firestore.instance.collection("mensagens").document().setData({""
+        "texto" : "Olá!",
+        "from" : "Parzival",
+        "read" : false
+    });*/
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue
+        )
+      ),
+      home: ChatScreen()
+    );
+  }
+}
