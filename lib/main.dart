68409cae244978dbc77606ed9cf592b273af3c6f
ParzivalOne <mateusdelg@gmail.com>
2020-03-01 21:29:04 -0300

    ignore working

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter demo",
+      title: "Flutter",
       theme: ThemeData(
         primarySwatch: Colors.blue,
         iconTheme: IconThemeData(
